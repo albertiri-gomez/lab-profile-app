0ead6b8bf5df3982d46b180273a5c738c54076b2
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2020-03-27 21:52:54 +0100

    iteration5 done

--- front/lib/auth.api.js
+++ front/lib/auth.api.js
@@ -32,12 +32,22 @@ export const doLogin = async ({ username, password }) => {
 export const doLogout = async () => {
   console.log("loggin out!");
   const res = await api.post("/logout");
-  console.log("respuesta del server al logout", res);
+  console.log(" logout", res);
 };
 
 export const whoUser = async () => {
   console.log("whoUser");
   const res = await api.post("/whoami");
-  console.log("respuesta del server al whami", res.data);
+  console.log("whami", res.data);
   return res.data;
+};
+
+export const doEdit = async ({ username, campus, course }) => {
+  console.log("edit!");
+  const res = await api.post("/edit", {
+    username,
+    campus,
+    course
+  });
+  console.log(" edit", res);
 };
--- front/src/pages/Profile.pages.js
+++ front/src/pages/Profile.pages.js
@@ -1,5 +1,82 @@
-import React from "react";
+import React, { useContext } from "react";
+import { withRouter } from "react-router-dom";
+import { doLogout, doEdit } from "../../lib/auth.api";
+import { useForm, FormContext } from "react-hook-form";
+import { ApiContext } from "../../context/ApiContext";
+import { InputBox } from "../components/Input";
 
-export const ProfilePage = () => {
-  return <div>profile</div>;
-};
+export const ProfilePage = withRouter(({ history }) => {
+  const { user, setUser } = useContext(ApiContext);
+
+  const onClickLogout = async e => {
+    e.preventDefault();
+    await doLogout();
+    setUser(null);
+    history.push("/");
+  };
+
+  const methods = user
+    ? useForm({
+        mode: "onBlur",
+        defaultValues: {
+          username: user?.username,
+          campus: user?.campus,
+          course: user?.course
+        }
+      })
+    : useForm({
+        mode: "onBlur"
+      });
+
+  console.log(user);
+  const { register, handleSubmit, errors } = methods;
+
+  const onEdit = async data => {
+    console.log("data", data);
+    await doEdit(data);
+    setUser(data);
+  };
+
+  return (
+    <FormContext {...methods}>
+      <>
+        <form onSubmit={handleSubmit(onEdit)}>
+          <div>
+            <label>Username</label>
+            <InputBox
+              // className={hasError(errors, "username")}
+              name="username"
+              ref={register({ required: true })}
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <InputBox
+              // className={hasError(errors, "password")}
+              name="password"
+              ref={register({ required: true })}
+            />
+          </div>
+          <div>
+            <label>Campus</label>
+            <InputBox
+              // className={hasError(errors, "campus")}
+              name="campus"
+              ref={register({ required: true })}
+            />
+          </div>
+          <div>
+            <label>Course</label>
+            <InputBox
+              // className={hasError(errors, "course")}
+              name="course"
+              ref={register({ required: true })}
+            />
+          </div>
+          <button type="submit">Editar Profile</button>
+          <button onClick={e => onClickLogout(e)}>Logout</button>
+        </form>
+      </>
+    </FormContext>
+  );
+});
